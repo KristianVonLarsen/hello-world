ac625a358d9355d08e5f7f7700387eb43cfc75f8
Kristian Larsen <kristian.larsen@rulestore.com>
2025-03-13 13:43:23 +0100

    Adding C#

---
+++ myDosApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myDosApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gotYa = true;
+        }
+    }
+}
